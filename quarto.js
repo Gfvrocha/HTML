d4140a34918a643a83439a3f252c234e0b9797c0
Felipe Rocha <felipe.viega.r@gmail.com>
2022-05-31 21:58:23 -0300

    Exercício Pesquisar Pokemon

---
+++ quarto.js
@@ -0,0 +1,12 @@
+function obterPokemons() {
+    var xhr = new XMLHttpRequest();
+    var nomePokemon = document.getElementById(`nomePokemon`).value;
+    xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
+    xhr.onload = function () {
+        document.getElementById(`resultado`).innerHTML=`<img src="${JSON.parse(xhr.response).sprites.front_default}">`
+        
+    };
+    xhr.send();
+
+}
+
